d3975688c7a69742461ac6e9972302c237b5a2c7
sprotg <spr@sde.dk>
2019-12-16 10:07:29 +0100

    animation

--- prøve_dec_2019/test.py
+++ prøve_dec_2019/test.py
@@ -2,6 +2,16 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
 from math import sin, pi
 import csv
+import imageio
+import datetime
+
+def create_gif(filenames, duration):
+    images = []
+    for filename in filenames:
+        images.append(imageio.imread(filename))
+    output_file = 'Gif-%s.gif' % datetime.datetime.now().strftime('%Y-%M-%d-%H-%M-%S')
+    imageio.mimsave(output_file, images, duration=duration)
+
 
 infile = open('gym-tal.csv', mode='r')
 reader = csv.DictReader(infile)
@@ -28,18 +38,41 @@ while not cmd.startswith('q'):
 
     elif cmd == 'gennemsnit':
         sum = 0
-        n : 0
-        for row in Data:
-            if row['Uddanelse'] == 'HTX':
+        n = 0
+        for row in data:
+            if row['Uddannelse'] == 'HTX':
                 for key, value in list(row.items())[1:]:
-                    sum += value
+                    sum += int(value)
                     n = n + 1
                 print("I 2012-2018 gik der i gennemsnit {} elever på HTX.".format(sum/n))
 
+    elif cmd == 'plot':
+        #Animation:
+        #https://www.idiotinside.com/2017/06/06/create-gif-animation-with-python/
+
+        #https://www.reddit.com/r/dataisbeautiful/comments/dsv88h/how_did_the_uks_regions_respond_to_the_2008_house/
+        fig, ax = plt.subplots()
+        for row in data:
+            if row['Uddannelse'] == 'HTX':
+                for n in range(1,8):
+                    tal = []
+                    aar = []
+                    for key, value in list(row.items())[1:]:
+                        if int(key) < int('2012') + n:
+                            tal.append(int(value))
+                        else:
+                            tal.append(0)
+                        aar.append(key)
+                    ax.set_ylim(0,15000)
+                    ax.bar(aar, tal, color='green')
+                    plt.savefig('{}.png'.format(n))
+                create_gif(['1.png','2.png','3.png','4.png','5.png','6.png','7.png','8.png'], 1)
+
+
     elif cmd == 'kunst':
         ellipser = []
-        for x in range(10):
-            for y in range(10):
+        for x in range(20):
+            for y in range(20):
                 vinkel = sin(0.1*x*y)*360/(2*pi)
                 ellipser.append(Ellipse([x, y], 1, 0.6, angle=vinkel))
 
@@ -48,8 +81,8 @@ while not cmd.startswith('q'):
         for e in ellipser:
             ax.add_artist(e)
 
-        ax.set_xlim(-1, 11)
-        ax.set_ylim(-1, 11)
+        ax.set_xlim(-1, 21)
+        ax.set_ylim(-1, 21)
         ax.set_frame_on(False)
         ax.set_xticks([])
         ax.set_yticks([])
