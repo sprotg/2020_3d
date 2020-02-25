09dd20642758f62015586770ed334ba680e0c2a6
sprotg <spr@sde.dk>
2020-02-25 09:38:22 +0100

    Flere frames

--- grafiske brugerflader/intro.py
+++ grafiske brugerflader/intro.py
@@ -6,16 +6,42 @@ def beregn():
 
 mainFrame = tk.Frame()
 
-mainFrame.pack()
 
-knap = tk.Button(mainFrame, text="Beregn", command=beregn)
+mainFrame.pack()
+V = tk.Frame(mainFrame)
+H = tk.Frame(mainFrame)
+HT = tk.Frame(H)
+HB = tk.Frame(H)
+
+V.pack(side = tk.LEFT)
+H.pack(side = tk.LEFT)
+HT.pack(side = tk.TOP)
+HB.pack(side = tk.TOP)
+
+
+
+
+knap = tk.Button(V, text="Beregn", command=beregn)
 knap.pack(side=tk.TOP, fill=tk.BOTH)
 
-tekst = tk.Entry(mainFrame, textvcariable='3')
+knap2 = tk.Button(V, text= "2")
+knap2.pack(side= tk.TOP, fill=tk.BOTH)
+
+tekst = tk.Entry(V)
 
 tekst.pack(side=tk.TOP, fill=tk.BOTH)
 
-outputLabel = tk.Label(mainFrame, text="0")
-outputLabel.pack(side=tk.TOP, fill=tk.BOTH)
+outputLabel = tk.Label(HT, text="0")
+outputLabel.pack(side=tk.LEFT, fill=tk.BOTH)
+
+outputLabel2 = tk.Label(HT, text="123123")
+outputLabel2.pack(side=tk.LEFT, fill=tk.BOTH)
+
+can = tk.Canvas(HB)
+can.pack(side=tk.LEFT, fill=tk.BOTH)
+can.create_rectangle(10,20,30,40, fill="blue")
+
+for i in range(10):
+    can.create_rectangle(25*i+30,20,30,40, fill="blue")
 
 mainFrame.mainloop()
