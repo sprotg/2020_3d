94c371b08f64ff08a7ba23ef9be2a37098d5e447
sprotg <spr@sde.dk>
2020-03-11 07:56:22 +0100

    bøger

--- grafiske brugerflader/books/Book_data.py
+++ grafiske brugerflader/books/Book_data.py
@@ -6,8 +6,23 @@ class Book:
         self.titel = ""
         self.forfatter = ""
         self.aarstal = 0
-        self.rating = 0
+        #[1 stjerne, 2 stjerner, 3 stjerner...]
+        self.ratings = [0,0,0,0,0]
         self.id = -1
+
+    def get_rating(self):
+        r = 0
+
+        for i in range(0,len(self.ratings)):
+            r += (i+1)*self.ratings[i]
+
+        return r/sum(self.ratings)
+
+    def give_rating(self, r):
+        if 0 <= r <= 5:
+            self.ratings[int(r)-1] += 1
+        else:
+            print("Fejl, rating er ikke gyldig")
 
 
 class Books_data:
@@ -26,11 +41,18 @@ class Books_data:
         self.books = []
         for book in reader:
             b = Book()
-            b.titel = book["title"]
-            b.rating = book["average_rating"]
-            b.aarstal = book["original_publication_year"]
-            b.forfatter = book["authors"]
-            b.id = int(book['book_id'])
+            try:
+                b.titel = book["title"]
+                b.ratings[0] = int(book["ratings_1"])
+                b.ratings[1] = int(book["ratings_2"])
+                b.ratings[2] = int(book["ratings_3"])
+                b.ratings[3] = int(book["ratings_4"])
+                b.ratings[4] = int(book["ratings_5"])
+                b.aarstal = book["original_publication_year"]
+                b.forfatter = book["authors"]
+                b.id = int(book['book_id'])
+            except:
+                print(book)
 
             self.books.append(b)
         print("Indlæst {} bøger".format(len(self.books)))
@@ -44,6 +66,14 @@ class Books_data:
         else:
             n = len(self.books)-1
         return self.books[0:n]
+
+    def slet_bog(self, b):
+        '''
+        Slet en bog med et bestemt id
+        '''
+        for book in self.books:
+            if book.id == b.id:
+                self.books.remove(book)
 
     def get_book(self, id):
         '''
@@ -63,5 +93,4 @@ class Books_data:
             if book.id == b.id:
                 book.forfatter = b.forfatter
                 book.titel = b.titel
-                book.rating = b.rating
                 book.aarstal = b.aarstal
