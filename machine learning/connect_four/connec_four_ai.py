da6c4f49530bd80fbe60e2c98b6ecd8ddfe8c374
sprotg <spr@sde.dk>
2021-02-04 09:31:06 +0100

    Fire på stribe

---
+++ machine learning/connect_four/connec_four_ai.py
@@ -0,0 +1,31 @@
+from random import randint
+import pickle
+from numpy import linalg
+
+class Connect_four_ai():
+
+    def __init__(self):
+        self.training_data = set()
+        self.add_data(([[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]],3))
+        self.load()
+        print("Loaded {} training examples".format(len(self.training_data)))
+
+    def make_move(self, game):
+        n = self.find_nearest(game.grid)
+        return n[1]
+
+    def add_data(self, move):
+        v = [item for sublist in move[0] for item in sublist]
+        self.training_data.add((tuple(v),move[1]))
+        print("{} training examples!".format(len(self.training_data)))
+
+    def load(self):
+        self.training_data = pickle.load(open("training_data.p", "rb"))
+
+    def save(self):
+        pickle.dump(self.training_data, open("training_data.p", "wb"))
+
+    def find_nearest(self, state):
+        flat = [item for sublist in state for item in sublist]
+        n = min(self.training_data, key=lambda x:linalg.norm([flat[i] - x[0][i] for i in range(len(flat))]))
+        return n
---
+++ machine learning/connect_four/game.py
@@ -0,0 +1,44 @@
+class Connect_four_game:
+
+    def __init__(self):
+        self.reset()
+        #States:
+        # 0: start
+        # 1: player 1
+        # 2: check for win?
+        # 3: player 2
+        # 4: check for win?
+        # 5: game over
+        # 6: reset game
+
+    def reset(self):
+        self.grid = [[0 for i in range(6)] for i in range(6)]
+        self.state = 0
+
+
+    def place(self, col):
+        if self.state == 1 or self.state == 3:
+            if self.grid[col][0] == 0:
+                #Valid move
+                i = 5
+                while self.grid[col][i] != 0:
+                    i -= 1
+                self.grid[col][i] = self.turn()
+                self.state += 1
+
+    def win(self):
+        #Check for win
+        #Lodret
+        for y in range(6):
+            for x in [0,1,2]:
+                if (self.grid[y][x] == self.grid[y][x+1] ==
+                self.grid[y][x+2] ==
+                self.grid[y][x+3]) and self.grid[y][x] != 0:
+                    return self.grid[y][x]
+        return 0
+
+    def turn(self):
+        if self.state < 3:
+            return 1
+        else:
+            return 2
