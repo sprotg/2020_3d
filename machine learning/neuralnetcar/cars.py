50f697228ef0b9f22cc93a82377c100fa4aaa686
sprotg <spr@sde.dk>
2021-02-09 11:30:44 +0100

    biler

--- machine learning/neuralnetcar/cars.py
+++ machine learning/neuralnetcar/cars.py
@@ -23,10 +23,10 @@ class Car():
     def update(self, inputs):
         self.steer = self.evaluate(inputs)
         self.dir += self.steer
-        if self.dir > 1:
-            self.dir = 1
-        if self.dir < -1:
-            self.dir = -1
+        if self.dir > 1.5:
+            self.dir = 1.5
+        if self.dir < -1.5:
+            self.dir = -1.5
         self.pos[0] += math.cos(self.dir)
         self.pos[1] += math.sin(self.dir)
 
@@ -40,9 +40,10 @@ class Car():
     def evaluate(self, inputs):
         #1/(1+e^{-x})
         x = self.dot(inputs,self.weights[0:len(inputs)]) + self.weights[-1]
+        print(x)
         L = 0.1
         k = 1
-        return (L/(1+math.exp(-k*x)))-0.5
+        return (L/(1+math.exp(-k*x)))-0.05
 
 
 
@@ -50,10 +51,11 @@ class Game():
     def __init__(self, width, height):
         self.w = width
         self.h = height
-        self.car = Car([random.random()*2-1 for i in range(6)])
+        self.car = Car([random.random()-0.5 for i in range(6)])
         self.latest_input = []
         self.points = []
         self.population = []
+        self.avg = 0
 
     def get_weights_by_selection(self):
         c1 = random.choice(self.population)
@@ -64,6 +66,8 @@ class Game():
                 weights[i] = c1[0][i]
             else:
                 weights[i] = c2[0][i]
+            if random.random() > 0.01:
+                weights[i] = random.random()-0.5
         return weights
 
     def update(self):
@@ -91,16 +95,20 @@ class Game():
 
         if self.car.pos[0] > 550 or bane.get_at((int(self.car.pos[0]),int(self.car.pos[1]))) != (0,0,0,255):
             self.population.append((self.car.weights, self.car.pos[0]))
-            if len(self.population) > 10:
+            if len(self.population) > 100:
                 self.population.sort(key = lambda x:x[1], reverse=True)
-                self.population = self.population[0:10]
+                self.population = self.population[0:100]
+                sum = 0
+                for c in self.population:
+                    sum += c[1]
+                self.avg = sum/100
 
 
-            print("Fitness: {}".format(self.car.pos[0]))
-            self.car = Car([random.random()*2-1 for i in range(6)])
-            if len(game.population) > 2 and random.random() > 0.5:
+            #print("Fitness: {}".format(self.car.pos[0]))
+            self.car = Car([random.random()-0.5 for i in range(6)])
+            if len(game.population) > 50 and random.random() > 0.5:
                 self.car.weights = game.get_weights_by_selection()
-                print("Selection!")
+                #print("Selection!")
 
 game = Game(500,500)
 
@@ -114,7 +122,11 @@ def draw_game():
 
     screen.blit(myfont.render("inputs: {}".format(game.latest_input), 0, (255,255,255)), (20,20))
     screen.blit(myfont.render("output: {:0.2f}".format(game.car.steer), 0, (255,255,255)), (20,40))
-    screen.blit(myfont.render("network: {}".format(game.car.weights), 0, (255,255,255)), (20,60))
+    #screen.blit(myfont.render("network: {}".format(game.car.weights), 0, (255,255,255)), (20,60))
+    screen.blit(myfont.render("speed: {}".format(speed), 0, (255,255,255)), (20,80))
+    screen.blit(myfont.render("Average fitness: {}".format(game.avg), 0, (255,255,255)), (20,100))
+    if len(game.population) > 0:
+        screen.blit(myfont.render("Max fitness: {}".format(game.population[0][1]), 0, (255,255,255)), (20,120))
 
     for p in game.points:
         pygame.draw.ellipse(screen, (200,100,200), pygame.Rect(p[0],p[1], 5, 5))
@@ -129,6 +141,7 @@ def output_logic(tilstand):
 def draw_menu():
     pass
 
+speed = 1
 
 #Main game loop
 while not done:
@@ -136,14 +149,19 @@ while not done:
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             done = True
         if (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
-            bane = pygame.image.load('bane1.png')
+            bane = pygame.image.load('bane3.png')
         if (event.type == pygame.KEYDOWN and event.key == pygame.K_w):
             bane = pygame.image.load('bane2.png')
+        if (event.type == pygame.KEYDOWN and event.key == pygame.K_a):
+            speed -= 1
+        if (event.type == pygame.KEYDOWN and event.key == pygame.K_s):
+            speed += 1
 
         #Håndtering af input fra mus
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
-    game.update()
+    for i in range(speed):
+        game.update()
 
     output_logic(tilstand)
 
